9e46c8b49d92834c109f3d831ed8238dcf185976
emilianocampos <emicampos1268@gmail.com>
2024-09-16 01:16:34 -0300

    4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,25 @@
 let aud = document.getElementById('audio'); // Elemento de audio
   let toggleButton = document.getElementById('sonido'); // Elemento que dispara el sonido
+let submit = document.getElementById('submit');
 
+function onAlert() {
+  // const Toast = Swal.mixin({
+  //   toast: true,
+  //   position: "top-end",
+  //   showConfirmButton: false,
+  //   timer: 3000,
+  //   timerProgressBar: true,
+  //   didOpen: (toast) => {
+  //     toast.onmouseenter = Swal.stopTimer;
+  //     toast.onmouseleave = Swal.resumeTimer;
+  //   }
+  // });
+  // Toast.fire({
+  //   icon: "success",
+  //   title: "Signed in successfully"
+  // });
+alert('mesn')
+}
   function play() {
     aud.play(); // Reproduce el audio
   }
@@ -16,3 +35,5 @@ let aud = document.getElementById('audio'); // Elemento de audio
   btn.addEventListener('click', () => {
    btn.classList.add('hinge animated')
   })
+
+  submit.addEventListener('click', () =>{alert('sad')})
